7e26d267d855d9d439dd9cd2a29745ca3c04eace
Program8 <appsgem123@gmail.com>
2023-10-27 21:49:05 +0530

    Initial Commit

---
+++ CommonCodeFW/CommonCodeFW.h
@@ -0,0 +1,18 @@
+//
+//  CommonCodeFW.h
+//  CommonCodeFW
+//
+//  Created by Vijay Sachan on 27/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonCodeFW.
+FOUNDATION_EXPORT double CommonCodeFWVersionNumber;
+
+//! Project version string for CommonCodeFW.
+FOUNDATION_EXPORT const unsigned char CommonCodeFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCodeFW/PublicHeader.h>
+
+
