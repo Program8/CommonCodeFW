3e6a19b938239833e65aec77761c373418e54b6f
Program8 <appsgem123@gmail.com>
2023-10-28 14:11:09 +0530

    --

--- CommonCodeFW/Main/Test123.swift
+++ CommonCodeFW/Main/Test123.swift
@@ -5,9 +5,12 @@
 //  Created by Vijay Sachan on 27/10/23.
 //
 import Foundation
-import FirebaseCrashlytics
-public class Test12{
+//import FirebaseCrashlytics
+public class Test123{
     public static func val()->String{
+        return "4234"
+    }
+    public static func val2()->String{
         return "4234"
     }
 }
